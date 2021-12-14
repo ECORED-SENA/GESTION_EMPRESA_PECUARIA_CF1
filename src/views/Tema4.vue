5a68b06f212e0ad8f1a3174ab007031bf80059cf
luisEfe80 <luismendez80@hotmail.com>
2021-12-13 22:49:10 -0500

    Adicion de contenido

---
+++ src/views/Tema4.vue
@@ -0,0 +1,93 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 4
+      h1 Organización de recursos
+
+    
+    #t_4_1.titulo-segundo.color-secundario
+      h2 4.1  Recursos técnicos
+
+
+    Separador
+    br
+    br
+    #t_4_2.titulo-segundo.color-secundario
+      h2 4.2  Recursos humanos
+
+
+    Separador
+    br
+    br
+    #t_4_3.titulo-segundo.color-secundario
+      h2 4.3  Recursos ambientales
+
+
+
+
+    Separador
+    br
+    br
+    #t_4_4.titulo-segundo.color-secundario
+      h2 4.4  Elaboración de cronogramas
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'Tema3',
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass"></style>
---
+++ src/views/Tema5.vue
@@ -0,0 +1,93 @@
+<template lang="pug">
+.curso-main-container.pb-3
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5
+
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 5
+      h1 Mantenimiento de equipos e instalaciones
+
+    
+    #t_5_1.titulo-segundo.color-secundario
+      h2 5.1  Manuales de operación de equipos
+
+
+    Separador
+    br
+    br
+    #t_5_2.titulo-segundo.color-secundario
+      h2 5.2  Protocolos de mantenimiento de equipos e instalaciones
+
+
+    Separador
+    br
+    br
+    #t_5_3.titulo-segundo.color-secundario
+      h2 5.3  Protocolos de desinfección
+
+
+
+
+    Separador
+    br
+    br
+    #t_5_4.titulo-segundo.color-secundario
+      h2 5.4  Manejo seguro de residuos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</template>
+
+<script>
+export default {
+  name: 'Tema3',
+  data: () => ({
+    // variables de vue
+  }),
+  mounted() {
+    this.$nextTick(() => {
+      this.$aosRefresh()
+    })
+  },
+  updated() {
+    this.$aosRefresh()
+  },
+}
+</script>
+
+<style lang="sass"></style>
