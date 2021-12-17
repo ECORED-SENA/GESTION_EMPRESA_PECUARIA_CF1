1b65406ead7cd70617ea095d083aa643e220168f
luisEfe80 <luismendez80@hotmail.com>
2021-12-17 17:14:45 -0500

    Adicion de contenido

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -152,6 +152,16 @@
 
     #t_2_1.titulo-segundo.color-secundario
       h2 2.1  Salud y seguridad en el trabajo
+    .row.mb-5
+      .col-lg-1
+        figure(data-aos='flip-left')
+          img(src='@/assets/curso/t1-ic1.svg' alt='imagen decorativa')        
+      .col-lg-11(data-aos='slide-left')
+        p.mb-0  Principales aspectos que se deben tener en cuenta respecto a la gestión integral de la salud y la seguridad en el trabajo. Parte del trabajo relacionado con la salud y la seguridad en el trabajo se relaciona con la identificación de los riesgos que conlleva cada una de las actividades realizadas. Es así como se definen diferentes tipos de riesgos, dependiendo de sus causas.
+
+    //-- Aqui viene la imagen infografica
+
+
 
 
     Separador
@@ -160,10 +170,103 @@
     #t_2_2.titulo-segundo.color-secundario
       h2 2.2  Manejo ambiental
 
+    .row.mb-5
+      .col-lg-7
+        p.mb-3
+          span.text-green Uno de los principios de la nueva forma de desarrollar explotaciones pecuarias es la protección de los recursos naturales y la sostenibilidad ambiental, por lo que este es un tema transversal a todas las actividades que tienen lugar en la unidad productiva. 
+          | Dentro del manejo ambiental existen elementos fundamentales sobre los cuales se desarrolla todo el ejercicio, por lo que dentro de la planificación de las actividades es importante definir cómo se abordarán y qué recursos serán necesarios para poderlos implementar. Afortunadamente, respecto al tema ambiental existe una normativa aplicable que ordena estas actividades y las integra al sistema productivo.
+      .col-lg-5
+        figure(data-aos='flip-left')
+          img(src='@/assets/curso/t2-i4.jpg' alt='imagen decorativa')      
 
-
-
-
+    .row.mb-5.justify-content-center
+      .col-lg-10
+        figcaption.customCaption  <b>Tabla 1</b><br> Marco normativo aspectos ambientales
+          .tabla-a.color-acento-botones.mb-5 
+            table
+              thead
+                tr
+                  th Norma
+                  th Alcance
+              tbody
+                tr
+                  td.text-bold Resolución 1476 septiembre 10 de 1976 ICA
+                  td Reglamenta la Resolución 261 de 1975, sobre el control de la salmonelosis en las aves de corral.
+                tr
+                  td.text-bold Resolución 1056 del 17 de abril de 1996 ICA
+                  td Control técnico de los insumos pecuario y se derogan las Resoluciones 710 de 1981, 2218 de 1980 y 444 de 1993.
+                tr
+                  td.text-bold Resolución 1698 del 27 de junio de 2000 ICA
+                  td Disposiciones sobre productores de alimentos para animales con destino al autoconsumo.
+                tr
+                  td.text-bold  Resolución 150 del 21 de enero de 2003 ICA
+                  td  Adopta el reglamento técnico de fertilizantes y acondicionadores de suelos para Colombia. 
+                tr
+                  td.text-bold  Resolución 968 del 10 de marzo de 2010 ICA 
+                  td  Modifica la Resolución 150 de 2003. 
+                tr
+                  td.text-bold  Ley 1255 del 28 de noviembre de 2008 ICA 
+                  td  Creación de un programa que preserve el estado sanitario de país libre de influenza aviar, así como el control y erradicación de la enfermedad Newcastle en el territorio nacional. 
+                tr
+                  td.text-bold  Resolución 587 del 27 de diciembre de 1973 ICA 
+                  td  Medidas para el control de la enfermedad de Mark en las aves de corral. 
+                tr
+                  td.text-bold  Resolución 1937 del 22 de julio de 2003 ICA 
+                  td Medidas sanitarias para la prevención y control de la enfermedad Newcastle en el territorio nacional.
+                tr
+                  td.text-bold  Resolución 375 del 27 de febrero de 2004 ICA 
+                  td  Disposiciones sobre Registro y Control de los Bioinsumos y Extractos Vegetales de uso agrícola en Colombia. 
+                tr
+                  td.text-bold  Resolución 2896 del 10 de octubre de 2005 ICA 
+                  td  Disposiciones sanitarias para la construcción de nuevas granjas avícolas en el territorio nacional. 
+                tr
+                  td.text-bold Resolución 3654 del 28 de septiembre de 2009 ICA
+                  td  Adopción del programa para el control y erradicación de la enfermedad Newcastle en el territorio nacional. 
+                tr
+                  td.text-bold  Resolución 1183 del 25 de marzo de 2010 ICA 
+                  td  Condiciones de Bioseguridad que se deben cumplir las granjas avícolas comerciales en el país para su certificación. 
+                tr
+                  td.text-bold  Resolución 4287 del 21 de noviembre de 2007 del Ministerio de la Protección Social
+                  td  Reglamento técnico sobre los requisitos sanitarios y de inocuidad de la carne y productos cárnicos comestibles de las aves de corral.  
+                tr
+                  td.text-bold Decreto 1500 del 4 de mayo de 2007 del Ministerio de la Protección Social 
+                  td  Creación del Sistema Oficial de Inspección, Vigilancia y Control de la Carne, Productos Cárnicos Comestibles y Derivados Cárnicos Destinados para el Consumo Humano - requisitos sanitarios y de inocuidad en la producción primaria. 
+                tr
+                  td.text-bold  Resolución 2101 del 27 de julio de 2007 ICA 
+                  td  Programas de seguridad alimentaria con fines comerciales de autoconsumo o cualquier programa enfocado hacia la distribución de aves. 
+                tr
+                  td.text-bold  Resolución 2833 del 29 de octubre de 2007 ICA 
+                  td  Modifica la Resolución 2101 de 2007. 
+                tr
+                  td.text-bold  Decreto 3100 del 30 de octubre de 2003 del Ministerio del Medio Ambiente Vivienda y Desarrollo Territorial 
+                  td  Reglamenta la tasa retributiva por la utilización directa del agua como receptor de los vertimientos puntuales y se toman otras determinaciones. 
+                tr
+                  td.text-bold  Decreto 1713 del 6 de agosto de 2002 del Ministerio del Medio Ambiente Vivienda y Desarrollo Territorial  
+                  td  Reglamenta la Ley 142 de 1994, la Ley 632 de 2000 y la Ley 689 del 2001, en relación con la prestación del servicio público de aseo, y el Decreto Ley 2811 de 1974, la Ley 99 de 1993 en relación con la Gestión Integral de Residuos Sólidos. 
+                tr
+                  td.text-bold Ley 55 del 2 de julio de 1993 del Ministerio del Trabajo y Seguridad Social 
+                  td Por medio de la cual se aprueba en “Convenio #170 y la recomendación número 177 sobre la seguridad en la utilización de productos químicos en el trabajo” adoptados por la 77 reunión de la conferencia general de la OIT.
+                tr
+                  td.text-bold  Decreto 1594 de 1984 del Ministerio de Agricultura  
+                  td  Reglamenta parcialmente el título I de la ley 9 de 1979, así como el capítulo II del título VI - parte III - libro II y el título III de la parte III - libro I – del Decreto 2811 de 1974 en cuanto a usos del agua y residuos líquidos. 
+                tr
+                  td.text-bold  Decreto 3172 de 2003 del Ministerio de Ambiente, Vivienda y Desarrollo Territorial 
+                  td  Reglamenta el artículo 158-2 del Estatuto Tributario. ”El contribuyente deberá acreditar el cumplimiento de los siguientes requisitos cuando la Dirección de Impuestos y Aduanas Nacionales, DIAN los exija por inversiones en control y mejoramiento del medio ambiente. 
+                tr
+                  td.text-bold  Decreto 1753 de 1994 del Ministerio del Medio Ambiente 
+                  td  Reglamenta parcialmente los títulos VIll y Xll de la Ley 99 de 1993 sobre licencias ambientales. 
+                tr
+                  td.text-bold  Ley 99 de 1993 del Congreso de la República  
+                  td  Por la cual se crea el Ministerio del Medio Ambiente, se reordena el sector público encargado de la gestión y conservación del medio ambiente y los recursos naturales renovables, se organiza el Sistema Nacional Ambiental, SINA. 
+                tr
+                  td.text-bold  Decreto 1900 de 2006 del Ministerio de Ambiente, Vivienda y Desarrollo Territorial  
+                  td  Reglamenta el parágrafo del artículo 43 de la Ley 99 de 993. Todo proyecto que involucre en su ejecución el uso del agua tomada directamente de fuentes naturales y que esté sujeto a la obtención de licencia ambiental, deberá destinar el 1% del total de la inversión para la recuperación, conservación, preservación y vigilancia de la cuenca hidrográfica que alimenta la respectiva fuente hídrica; de conformidad con el parágrafo del artículo 43 de la Ley 99 de 1993. 
+                tr
+                  td.text-bold  Resolución 1023 de 2005 del Ministerio de Ambiente y Desarrollo Territorial 
+                  td  Por la cual se adoptan guías ambientales como instrumento de autogestión y autorregulación. 
+                tr
+                  td.text-bold  Decreto 1299 de 2008 del Ministerio de Ambiente y Desarrollo Territorial 
+                  td  Por el cual se reglamenta el departamento de gestión ambiental de las empresas a nivel industrial y se dictan otras disposiciones. 
 
 
 
